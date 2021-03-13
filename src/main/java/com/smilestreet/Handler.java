730e036f44aaad4eaf3303d14abfeaf075c585cf
Ariel Pillemer <pillemer@gmail.com>
2021-03-13 18:31:22 +0000

    java on vscode no worky worky

--- src/main/java/com/smilestreet/Handler.java
+++ src/main/java/com/smilestreet/Handler.java
@@ -2,6 +2,7 @@ package com.smilestreet;
 
 import java.util.Collections;
 import java.util.Map;
+import java.util.ArrayList;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -11,16 +12,18 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 
 public class Handler implements RequestHandler<Map<String, Object>, ApiGatewayResponse> {
 
-	private static final Logger LOG = LogManager.getLogger(Handler.class);
-
 	@Override
 	public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
-		LOG.info("received: {}", input);
-		Response responseBody = new Response("Go Serverless v1.x! Your function executed successfully!", input);
+
+		User user1 = new User(0001, "ariel", "pillemer", 016404151, "pillemer@gmail.com", "unemployed", "Wales", "2", "12/03/2021", "31/12/2022");
+		User user2 = new User(0001, "Taslima", "Patel", 3207213, "Taslima@gmail.com", "Greeat!", "Manchester", "1", "18/03/2021", "22/10/2021");
+		List<User> users = new ArrayList<>();
+		users.add(user1);
+		users.add(user2);
+
 		return ApiGatewayResponse.builder()
 				.setStatusCode(200)
-				.setObjectBody(responseBody)
-				.setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & serverless"))
+				.setObjectBody(users)
 				.build();
 	}
 }
---
+++ src/main/java/com/smilestreet/model/User.java
@@ -0,0 +1,110 @@
+
+package com.smilestreet.model;
+
+public class User {
+    
+    private int volunteerId;
+    private String firstname;
+    private String lastname;
+    private String contactnumber;
+    private String username;
+    private String employername;
+    private String primarylocation;
+    private String numberofdays;
+    private String startdate;
+    private String enddate;
+
+    public User(int volunteerId, String firstname, String lastname, String contactnumber, String username, String employername, String primarylocation, String numberofdays, String startdate, String enddate) {
+        this.volunteerId = volunteerId;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.contactnumber = contactnumber;
+        this.username = username;
+        this.employername = employername;
+        this.primarylocation = primarylocation;
+        this.numberofdays = numberofdays;
+        this.startdate = startdate;
+        this.enddate = enddate;
+    }
+
+    public int getVolunteerId() {
+        return this.volunteerId;
+    }
+
+    public void setVolunteerId(int volunteerId) {
+        this.volunteerId = volunteerId;
+    }
+
+    public String getFirstname() {
+        return this.firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return this.lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getContactnumber() {
+        return this.contactnumber;
+    }
+
+    public void setContactnumber(String contactnumber) {
+        this.contactnumber = contactnumber;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmployername() {
+        return this.employername;
+    }
+
+    public void setEmployername(String employername) {
+        this.employername = employername;
+    }
+
+    public String getPrimarylocation() {
+        return this.primarylocation;
+    }
+
+    public void setPrimarylocation(String primarylocation) {
+        this.primarylocation = primarylocation;
+    }
+
+    public String getNumberofdays() {
+        return this.numberofdays;
+    }
+
+    public void setNumberofdays(String numberofdays) {
+        this.numberofdays = numberofdays;
+    }
+
+    public String getStartdate() {
+        return this.startdate;
+    }
+
+    public void setStartdate(String startdate) {
+        this.startdate = startdate;
+    }
+
+    public String getEnddate() {
+        return this.enddate;
+    }
+
+    public void setEnddate(String enddate) {
+        this.enddate = enddate;
+    }
+    
+}
