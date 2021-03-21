68759c4c120f1353c067cf021b862dc2db895dd7
Ariel Pillemer <pillemer@gmail.com>
2021-03-21 13:32:38 +0000

    add UUID to saveGoodCauseHandler

--- src/main/java/com/smilestreet/SaveGoodCauseHandler.java
+++ src/main/java/com/smilestreet/SaveGoodCauseHandler.java
@@ -6,7 +6,6 @@ import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.smilestreet.model.SaveGoodCause;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -14,6 +13,7 @@ import java.io.IOException;
 import java.sql.*;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 public class SaveGoodCauseHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
@@ -27,7 +27,7 @@ public class SaveGoodCauseHandler implements RequestHandler<APIGatewayProxyReque
     public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent request, Context context) {
         LOG.info("received the request");
 
-        //  String volunteerId = request.getPathParameters().get("volunteer_id");
+        String goodCauseId = UUID.randomUUID().toString(); // create a unique ID for a newly registered user
         String requestBody = request.getBody();
         ObjectMapper objMapper = new ObjectMapper();
         APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent();
@@ -35,6 +35,7 @@ public class SaveGoodCauseHandler implements RequestHandler<APIGatewayProxyReque
         Map<String, String> headers = new HashMap<>();
         headers.put("Access-Control-Allow-Origin", "*");
         response.setHeaders(headers);
+        response.setBody(goodCauseId);
 
         try {
             SaveGoodCause v = objMapper.readValue(requestBody, SaveGoodCause.class);
@@ -47,19 +48,21 @@ public class SaveGoodCauseHandler implements RequestHandler<APIGatewayProxyReque
                     System.getenv("DB_USER"),
                     System.getenv("DB_PASSWORD")));
 
-            preparedStatement = connection.prepareStatement("INSERT INTO good_cause (firstname, lastname, emailaddress, contactnumber) VALUES (  ?, ?,?,?)");
-            // preparedStatement.setInt(1, v.getVolunteer_id());
-            //preparedStatement.setInt(2, Integer.parseInt(volunteerId));
-            preparedStatement.setString(1, v.getFirstname());
-            preparedStatement.setString(2, v.getLastname());
-            preparedStatement.setString(3, v.getEmailaddress());
-            preparedStatement.setString(4, v.getContactnumber());
+            preparedStatement = connection.prepareStatement("INSERT INTO good_cause (" +
+                    "good_cause_id, " +
+                    "firstname, " +
+                    "lastname, " +
+                    "emailaddress, " +
+                    "contactnumber) " +
+                    "VALUES (?, ?, ?, ?, ?)");
 
-
-
+            preparedStatement.setString(1, goodCauseId);
+            preparedStatement.setString(2, v.getFirstname());
+            preparedStatement.setString(3, v.getLastname());
+            preparedStatement.setString(4, v.getEmailaddress());
+            preparedStatement.setString(5, v.getContactnumber());
 
             preparedStatement.execute();
-
             connection.close();
         } catch (IOException e) {
             LOG.error("Unable to unmarshal JSON for adding a task", e);
