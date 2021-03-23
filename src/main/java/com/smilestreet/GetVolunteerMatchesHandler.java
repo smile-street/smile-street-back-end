2f2649c75a2461f48344ea955b6014cb9ac85ba5
shinesha <jodrew_7@hotmail.com>
2021-03-23 15:55:56 +0000

    copied accross a get request handler to work with

---
+++ src/main/java/com/smilestreet/GetVolunteerMatchesHandler.java
@@ -0,0 +1,67 @@
+package com.smilestreet;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.smilestreet.model.Good_cause;
+
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GetVolunteerMatchesHandler {
+    private ResultSet resultSet=null;
+
+    @Override
+    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent request, Context context) {
+        List<Good_cause> good_causes = null;
+        try {
+            good_causes = new ArrayList<>();
+            Class.forName("com.mysql.jdbc.Driver");
+
+            connection = DriverManager.getConnection(String.format("jdbc:mysql://%s/%s?user=%s&password=%s",
+                    System.getenv("DB_HOST"),
+                    System.getenv("DB_NAME"),
+                    System.getenv("DB_USER"),
+                    System.getenv("DB_PASSWORD")
+            ));
+            preparedStatement = connection.prepareStatement("SELECT * FROM  good_cause");
+            resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                Good_cause good_cause = new Good_cause(resultSet.getInt("good_cause_id"),
+                        resultSet.getString("descriptionofgoodcause"),
+                        resultSet.getString("firstname"),
+                        resultSet.getString("lastname"),
+                        resultSet.getString("emailaddress"),
+                        resultSet.getString("contactnumber"));
+
+                good_causes.add(good_cause);
+
+            }
+        } catch (Exception e) {
+            LOG.error(String.format("unable to query database"));
+        } finally {
+            closeConnection();
+        }
+        return null;
+    }
+
+
+    private void closeConnection() {
+        try {
+            if (resultSet != null) {
+                resultSet.close();
+            }
+            if (preparedStatement != null) {
+                preparedStatement.close();
+            }
+            if (connection != null) {
+                connection.close();
+            }
+        } catch (Exception e) {
+            LOG.error("unable to close connection", e.getMessage());
+        }
+    }
+}
+}
