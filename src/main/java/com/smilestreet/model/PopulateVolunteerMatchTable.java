317cbb46e881253d4505337687cc8e92ff246737
taslima26 <banglawalas@hotmail.co.uk>
2021-03-28 03:57:53 +0100

    added java class constructors,getters and setters

--- src/main/java/com/smilestreet/model/PopulateVolunteerMatchTable.java
+++ src/main/java/com/smilestreet/model/PopulateVolunteerMatchTable.java
@@ -1,5 +1,44 @@
 package com.smilestreet.model;
 
 public class PopulateVolunteerMatchTable {
-    
+    private int voln_id;
+    private int join_id;
+    private Boolean matched;
+
+    public PopulateVolunteerMatchTable(int voln_id, int join_id, Boolean matched) {
+        this.voln_id = voln_id;
+        this.join_id = join_id;
+        this.matched = matched;
+    }
+
+    public PopulateVolunteerMatchTable()
+    {
+
+    }
+
+    public int getVoln_id() {
+        return voln_id;
+    }
+
+    public void setVoln_id(int voln_id) {
+        this.voln_id = voln_id;
+    }
+
+    public int getJoin_id() {
+        return join_id;
+    }
+
+    public void setJoin_id(int join_id) {
+        this.join_id = join_id;
+    }
+
+    public Boolean getMatched() {
+        return matched;
+    }
+
+    public void setMatched(Boolean matched) {
+        this.matched = matched;
+    }
 }
+
+
